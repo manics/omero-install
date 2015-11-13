7706627bd5f275e5f44b330894c2035e8e33e983
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-11-13 23:22:35 +0000

    Add option to start apache

--- linux/install-centos7.sh
+++ linux/install-centos7.sh
@@ -2,6 +2,23 @@
 
 set -e -u -x
 
+WEBSERVER=nginx
+OMEROVER=omero
+
+for arg in "$@"; do
+	case "$arg" in
+	nginx|apache)
+		WEBSERVER="$arg"
+		;;
+	omero|omerodev|omerodevmerge)
+		OMEROVER="$arg"
+		;;
+	*)
+		echo "Unknown option: $arg";
+		exit 1
+	esac
+done
+
 source settings.env
 
 bash -eux dependencies-centos7.sh
@@ -9,10 +26,27 @@ bash -eux dependencies-centos7.sh
 bash -eux system_setup.sh
 bash -eux setup_postgres.sh
 
-cp settings.env setup_omero.sh ~omero
-su - omero -c "bash -eux setup_omero.sh"
+cp settings.env setup_$OMEROVER.sh ~omero
+if [ $WEBSERVER = apache ]; then
+	cp setup_omero_apache.sh ~omero
+fi
+if [ $OMEROVER = omerodev ]; then
+	yum -y install python-virtualenv
+	yum clean all
+	su - omero -c "bash -eux setup_$OMEROVER.sh"
+elif [ $OMEROVER = omerodevmerge ]; then
+	yum -y install python-virtualenv
+	yum clean all
+	su - omero -c "bash -eux setup_$OMEROVER.sh merge"
+fi 
 
-bash -eux setup_nginx_centos7.sh
+
+if [ $WEBSERVER = nginx ]; then
+	bash -eux setup_nginx_centos7.sh
+else
+	su - omero -c "bash -eux setup_omero_apache.sh"
+	bash -eux setup_apache_centos7.sh
+fi
 
 #If you don't want to use the systemd scripts you can start OMERO manually:
 #su - omero -c "OMERO.server/bin/omero admin start"
@@ -20,5 +54,5 @@ bash -eux setup_nginx_centos7.sh
 
 bash -eux setup_omero_daemon_centos7.sh
 
-#systemctl start omero.service
-#systemctl start omero-web.service
+systemctl start omero.service
+systemctl start omero-web.service
