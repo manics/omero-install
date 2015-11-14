830c3cafb6a9f51e051e071788aa1c66b0bdcb67
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-11-14 00:06:15 +0000

    add command to run different version of apache

--- linux/setup_omero_apache.sh
+++ linux/setup_omero_apache.sh
@@ -4,6 +4,19 @@ set -e -u -x
 
 source settings.env
 
+WEBSERVER=apache24
+
+for arg in "$@"; do
+	case "$arg" in
+	apache24)
+		WEBSERVER="$arg"
+		;;
+	*)
+		echo "Unknown option: $arg";
+		exit 1
+	esac
+done
+
 OMERO.server/bin/omero config set omero.web.application_server wsgi
-OMERO.server/bin/omero web config apache --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
+OMERO.server/bin/omero web config $WEBSERVER --http "$OMERO_WEB_PORT" > OMERO.server/apache.conf.tmp
 OMERO.server/bin/omero web syncmedia
